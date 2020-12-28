038194e02410cf73023ac003e5559163394647c9
Jacob Hampton <jhampton792@gmail.com>
2020-12-28 16:32:09 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,7 +1,19 @@
 # require modules here
+require 'yaml'
 
-def load_library
-  # code goes here
+def load_library(file_path)
+  emoticon_list = YAML.load(File.read(file_path))
+  
+  emoticon_hash = {}
+  emoticon_list.each do |key, value|
+    emoticon_hash[key] = {
+      english: emoticon_list[key][0],
+      japanese: emoticon_list[key][1]
+    }
+  end
+  
+  emoticon_hash
+  
 end
 
 def get_japanese_emoticon
