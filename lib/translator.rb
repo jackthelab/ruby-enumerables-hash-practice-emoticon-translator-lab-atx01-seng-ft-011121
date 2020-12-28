3549942d257ceb8ceefe22745e573f5c2b56de23
Jacob Hampton <jhampton792@gmail.com>
2020-12-28 16:38:06 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -25,6 +25,8 @@ def get_japanese_emoticon(file_path, emoticon)
       return working_lib[key][:japanese]
     end
   end
+  
+  "Sorry, that emoticon was not found"
 end
 
 def get_english_meaning(file_path, emoticon)
